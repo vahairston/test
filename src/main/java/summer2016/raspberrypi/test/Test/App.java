8ec33732de68b4040db0fe830c2b71c529cf4fab
Victoria Hairston <vhairs16@vt.edu>
2016-06-01 14:08:08 -0400

    Test eclipse file

---
+++ src/main/java/summer2016/raspberrypi/test/Test/App.java
@@ -0,0 +1,13 @@
+package summer2016.raspberrypi.test.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
